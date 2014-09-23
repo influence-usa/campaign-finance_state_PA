9a14abdc9dac58b32e6664eb4365bf94f2c95dcb
Bob Lannon <blannon@gmail.com>
2014-09-23 09:47:14 -0400

    updated init

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -1,5 +1,6 @@
 from utils import log
 from download import download_dos, download_cfo
-from extract import extract_cfo
-# from transform import
+from extract import extract_cfo, extract_dos
+from transform import transform_cfo
 # from validate import
+from describe import describe_dos
